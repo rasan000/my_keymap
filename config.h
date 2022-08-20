706a129f40f736e7d3998fd3b1056902a8621629
hakoiri <rasan000@me.com>
2022-08-20 20:36:14 +0900

    pin

--- config.h
+++ config.h
@@ -20,27 +20,37 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-// place overrides here 左手はD2で右手がD3
+// place overrides here
 
-#define MATRIX_ROW_PINS {D1, D0, D4, C6}
-#define MATRIX_COL_PINS {D7, E6, B4, B5, B6, B3, B1}
-#define MATRIX_COL_PINS {D7, E6, B4, B5, B6, B3, B1}
-// when write left
-#define SOFT_SERIAL_PIN D2
-// when write right
-// #define SOFT_SERIAL_PIN D3
+#define MATRIX_ROW_PINS \
+    { D1, D0, D4, C6 }
+
+// left keymap
+#define MATRIX_COL_PINS \
+    { D7, E6, B4, B5, B6, B3, B1 }
+
+// right keymap
+#define MATRIX_COL_PINS_RIGHT \
+    { B1, B3, B6, B5, B4, E6, D7 }
+
+// right keymap
+// #define MATRIX_COL_PINS_RIGHT
+//     { B1, B3, B4, B5, E6, B6, D7 }
+
+// when write left D2,when write right D3
+#define SOFT_SERIAL_PIN D3
 
 // If you need more program area, try select and reduce rgblight modes to use.
 
 // Selection of RGBLIGHT MODE to use.
 #if defined(LED_ANIMATIONS)
-   #define RGBLIGHT_EFFECT_BREATHING
-   #define RGBLIGHT_EFFECT_RAINBOW_MOOD
-   #define RGBLIGHT_EFFECT_RAINBOW_SWIRL
-   #define RGBLIGHT_EFFECT_SNAKE
-   #define RGBLIGHT_EFFECT_KNIGHT
-   #define RGBLIGHT_EFFECT_CHRISTMAS
-   #define RGBLIGHT_EFFECT_STATIC_GRADIENT
-   //#define RGBLIGHT_EFFECT_RGB_TEST
-   //#define RGBLIGHT_EFFECT_ALTERNATING
+#    define RGBLIGHT_EFFECT_BREATHING
+#    define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#    define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#    define RGBLIGHT_EFFECT_SNAKE
+#    define RGBLIGHT_EFFECT_KNIGHT
+#    define RGBLIGHT_EFFECT_CHRISTMAS
+#    define RGBLIGHT_EFFECT_STATIC_GRADIENT
+//#define RGBLIGHT_EFFECT_RGB_TEST
+//#define RGBLIGHT_EFFECT_ALTERNATING
 #endif
