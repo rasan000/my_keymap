1cd19789f68db861fc606563ad843f8e83a65b6d
hakoiri <rasan000@me.com>
2022-09-07 01:28:20 +0900

    記号関係をレイヤー1にすべて移動

--- keymap.c
+++ keymap.c
@@ -182,29 +182,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
       KC_ESC,KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                     KC_Y,    KC_U,   KC_I, KC_O, KC_P,KC_BSPC,
-      LCTL_T(KC_TAB), KC_A,KC_S, KC_D,    KC_F,    KC_G,                                                   KC_H,    KC_J,   KC_K, KC_L,KC_SCLN,KC_QUOT,
-      KC_LSFT,KC_Z, KC_X,    KC_C,   KC_V,     KC_B,                                                       KC_N,    KC_M, KC_COMM,KC_DOT,KC_UP,KC_MINS,
-      ADJUST,KC_F2,KC_F12,KC_LALT,KC_LGUI,LT(_LOWER,KC_MHEN),LCTL_T(KC_SPC),   RSFT_T(KC_ENT),LT(_RAISE,KC_HENK),KC_SLSH,KC_BSLS,KC_LEFT,KC_DOWN,KC_RGHT
+      LCTL_T(KC_TAB), KC_A,KC_S, KC_D,    KC_F,    KC_G,                                                   KC_H,    KC_J,   KC_K, KC_L,KC_MINS,KC_EQL,
+      KC_LSFT,KC_Z, KC_X,    KC_C,   KC_V,     KC_B,                                                       KC_N,    KC_M, KC_LCBR,KC_RCBR,KC_SCLN,KC_QUOT,
+      ADJUST,KC_F2,KC_F12,KC_LALT,KC_LGUI,LT(_LOWER,KC_SPC),LCTL_T(KC_SPC),   RCTL_T(KC_ENT),LT(_RAISE,KC_ENT),KC_GRV,KC_COMM,KC_DOT,KC_SLSH,KC_BSLS
   ),
 
   [_RAISE] = LAYOUT(
-      KC_TILD,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,                                 KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,KC_GRV,
-      _______,KC_1,KC_2,KC_3,KC_4,KC_5,                                             KC_PLUS,KC_EQL,KC_LCBR,KC_RCBR,KC_COLN,KC_DQT,
-      _______,KC_6,KC_7,KC_8,KC_9,KC_0,                                             KC_LBRC,KC_RBRC, KC_LT, KC_GT,_______,KC_UNDS,
-      _______, _______, _______, _______, _______, ADJUST, _______,       _______, _______, KC_QUES, KC_PIPE, _______, _______, _______
+      _______,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,                                KC_AMPR, KC_ASTR,KC_UP,KC_LPRN, KC_RPRN,KC_PIPE,
+      _______,_______, _______, _______, _______,_______,                            KC_CIRC,KC_LEFT,KC_DOWN,KC_RGHT,KC_UNDS,KC_PLUS,
+      _______,_______, _______, _______, _______,_______,                          _______, _______,KC_LBRC,KC_RBRC,KC_COLN,KC_DQUO,
+      _______, _______, _______, _______, _______, ADJUST, _______,       _______, _______, KC_TILD,KC_LT, KC_GT, KC_QUES, KC_PIPE
   ),
 
   [_LOWER] = LAYOUT(
-      KC_BSPC,KC_PSLS, KC_P7,KC_P8,KC_P9,KC_EQL,                            _______,LALT(KC_LEFT), KC_MS_U,LALT(KC_RGHT),_______,KC_DEL,
-      KC_LCAP,KC_PAST,KC_P4,KC_P5,KC_P6,KC_MINS,	                        _______,KC_MS_L, KC_MS_D,KC_MS_R,_______, _______,
-      KC_ENT,KC_0,KC_P1,KC_P2,KC_P3,KC_PLUS,                          _______, _______,_______, _______, KC_PGUP, _______,
-      KC_NLCK,_______,KC_COMM,KC_DOT,_______,_______,_______,     KC_MS_BTN1,ADJUST,KC_MS_BTN2,_______,KC_HOME,KC_PGDN,KC_END
+      _______,KC_P1,KC_P2,KC_P3,KC_P4,KC_P5,                         KC_P6,KC_P7, KC_MS_U,KC_P8,KC_P9,KC_P0,
+      KC_NUM,KC_P6,KC_P7,KC_P8,KC_P9,KC_P0,                         _______,KC_MS_L,KC_MS_D,KC_MS_R,_______, _______,
+      _______, _______, _______, _______,_______, _______,           _______, _______,_______,_______, _______, _______,
+      _______,_______,_______, _______,_______, _______,_______,     KC_MS_BTN1,ADJUST,KC_MS_BTN2,_______,_______,_______,_______
   ),
 
   [_ADJUST] = LAYOUT(
-      KC_F1, KC_F2,   KC_F3,   KC_F4, KC_F5,KC_F6,                         _______, LALT(KC_LEFT), KC_WH_U,LALT(KC_RGHT), _______, KC_DEL,
-      KC_F7,KC_F8,   KC_F9,   KC_F10, KC_F11, KC_F12,                         _______, KC_WH_L, KC_WH_D, KC_WH_R, _______, _______,
-      _______, KC_INS, KC_PSCR, KC_PAUS, _______, _______,                       _______, _______, _______, _______,_______, _______,
+      _______, KC_F1,KC_F2,   KC_F3,   KC_F4, KC_F5,                          _______,LALT(KC_LEFT),KC_WH_U,LALT(KC_RGHT), _______,KC_DEL,
+      KC_LCAP, KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,                                _______, KC_HOME, KC_WH_D, KC_END, _______, _______,
+      _______, KC_INS, KC_PSCR, KC_PAUS, KC_F11, KC_F12,                      _______, _______, _______, _______,_______, _______,
       _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______,  _______, _______,  _______, _______, _______
   ),
 };
@@ -234,7 +234,8 @@ void persistent_default_layer_set(uint16_t default_layer) {
   default_layer_set(default_layer);
 }
 
-// Setting ADJUST layer RGB back to default
+// Setting ADJUST layer RGB back to defaulte
+
 void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
   if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
     #ifdef RGBLIGHT_ENABLE
