f8bddd6fde0e9a2d28a0bf2a1fc39ff2190b66ad
hakoiri <rasan000@me.com>
2022-09-07 22:35:15 +0900

    修正

--- keymap.c
+++ keymap.c
@@ -181,17 +181,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
-      KC_ESC,KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                     KC_Y,    KC_U,   KC_I, KC_O, KC_P,KC_BSPC,
-      LCTL_T(KC_TAB), KC_A,KC_S, KC_D,    KC_F,    KC_G,                                                   KC_H,    KC_J,   KC_K, KC_L,KC_EQL,KC_MINS,
-      KC_LSFT,KC_Z, KC_X,    KC_C,   KC_V,     KC_B,                                                       KC_N,    KC_M,KC_COMM,KC_DOT,KC_SCLN,KC_QUOT,
-      ADJUST,KC_F2,KC_F12,KC_LALT,KC_LGUI,LT(_LOWER,KC_SPC),LCTL_T(KC_SPC),   RCTL_T(KC_ENT),LT(_RAISE,KC_ENT), KC_LCBR,KC_RCBR,KC_GRV,KC_SLSH,KC_BSLS
+      KC_ESC,KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                     KC_Y,    KC_U,   KC_I, KC_O, KC_P,KC_BSPC,
+      LCTL_T(KC_TAB), KC_A,KC_S, KC_D,    KC_F,    KC_G,                                   KC_H,    KC_J,   KC_K, KC_L,KC_SCLN,KC_QUOT,
+      KC_LSFT,KC_Z, KC_X,    KC_C,   KC_V,     KC_B,                                       KC_N,    KC_M,KC_COMM,KC_DOT,KC_EQL,KC_MINS,
+      ADJUST,KC_F2,KC_F12,KC_LALT,KC_LGUI,LT(_LOWER,KC_SPC),LCTL_T(KC_SPC),   KC_ENT,RAISE,KC_SLSH,KC_LCBR,KC_RCBR,KC_GRV,KC_BSLS
   ),
 
   [_RAISE] = LAYOUT(
       _______,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,                                KC_AMPR, KC_ASTR,KC_UP,KC_LPRN, KC_RPRN,KC_PIPE,
-      _______,_______, _______, _______, _______,_______,                            KC_CIRC,KC_LEFT,KC_DOWN,KC_RGHT,KC_PLUS,KC_UNDS,
-      _______,_______, _______, _______, _______,_______,                          _______, _______,KC_LT, KC_GT,KC_COLN,KC_DQUO,
-      _______, _______, _______, _______, _______, ADJUST, _______,       _______, _______, KC_LBRC,KC_RBRC,KC_TILD,KC_QUES, KC_PIPE
+      _______,_______, _______, _______, _______,_______,                            KC_CIRC,KC_LEFT,KC_DOWN,KC_RGHT,KC_COLN,KC_DQUO,
+      _______,_______, _______, _______, _______,_______,                          _______, _______,KC_LT, KC_GT,KC_PLUS,KC_UNDS,
+      _______, _______, _______, _______, _______, ADJUST, _______,       _______, _______,KC_QUES, KC_LBRC,KC_RBRC,KC_TILD, KC_PIPE
   ),
 
   [_LOWER] = LAYOUT(
