63f2c9026185a753a22a370f05614cc32b5494c4
hakoiri <rasan000@me.com>
2022-09-10 01:26:51 +0900

    hjkl形式に変更

--- keymap.c
+++ keymap.c
@@ -181,31 +181,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_QWERTY] = LAYOUT(
-      KC_ESC,KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                              KC_Y,    KC_U,   KC_I, KC_O, KC_P,KC_BSPC,
-      LCTL_T(KC_TAB), KC_A,KC_S, KC_D,    KC_F,    KC_G,                                            KC_H,    KC_J,   KC_K, KC_L,KC_SCLN,KC_QUOT,
-      KC_LSFT,KC_Z, KC_X,    KC_C,   KC_V,     KC_B,                                                KC_N,    KC_M,KC_COMM,KC_DOT,KC_EQL,KC_MINS,
-      ADJUST,KC_F2,KC_F12,KC_LALT,KC_LGUI,LT(_LOWER,KC_SPC),LCTL_T(KC_SPC),  RSFT_T(KC_ENT),RAISE,KC_SLSH,KC_LCBR,KC_RCBR,KC_GRV,KC_BSLS
+      KC_ESC,KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                          KC_Y, KC_U,   KC_I,  KC_O,KC_P,KC_BSPC,
+      LCTL_T(KC_TAB), KC_A,KC_S, KC_D,    KC_F,    KC_G,                                        KC_H, KC_J,   KC_K,  KC_L,KC_SCLN,KC_QUOT,
+      KC_LSFT,KC_Z, KC_X,    KC_C,   KC_V,     KC_B,                                            KC_N, KC_M,KC_COMM,KC_DOT,KC_SLSH,KC_MINS,
+      ADJUST,KC_F2,KC_F12,KC_LALT,KC_LGUI,LT(_LOWER,KC_SPC),CTL_T(KC_SPC),  SFT_T(KC_ENT),RAISE,RALT(KC_GRV),KC_LEFT,KC_DOWN,KC_UP,LT(_ADJUST,KC_RGHT)
   ),
 
   [_RAISE] = LAYOUT(
-      _______,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,                           KC_AMPR,KC_ASTR,KC_UP,KC_LPRN, KC_RPRN,KC_PIPE,
-      _______,_______,_______,_______,_______,_______,                        KC_CIRC,KC_LEFT,KC_DOWN,KC_RGHT,KC_COLN,KC_DQUO,
-      _______,_______,_______,_______,_______,_______,                        _______,_______,KC_LT, KC_GT,KC_PLUS,KC_UNDS,
-      _______,_______,_______,_______,_______,ADJUST, _______,       _______, _______,KC_QUES, KC_LBRC,KC_RBRC,KC_TILD, KC_PIPE
+      _______,KC_EXLM,KC_AT,KC_HASH,KC_DLR,KC_PERC,                           KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,_______,
+      KC_CAPS,KC_GRV,KC_TILD,_______,KC_LCBR,KC_RCBR,                         KC_LEFT,KC_DOWN,KC_UP,KC_RGHT,KC_COLN,KC_DQUO,
+      _______,KC_BSLS,KC_PIPE,_______,KC_LBRC,KC_RBRC,                        KC_PLUS,KC_EQL,KC_LT, KC_GT,KC_QUES,KC_UNDS,
+      _______,_______,_______,_______,_______,ADJUST, _______,       _______, _______,_______,_______,_______, _______,_______
   ),
 
   [_LOWER] = LAYOUT(
-      _______,KC_P1,KC_P2,KC_P3,KC_P4,KC_P5,                                    KC_P6,KC_P7,KC_MS_U,KC_P8,KC_P9,KC_P0,
-      KC_NUM,KC_P6,KC_P7,KC_P8,KC_P9,KC_P0,                                     _______,KC_MS_L,KC_MS_D,KC_MS_R,  _______,_______,
-      _______, _______, _______, _______,_______, _______,                      _______,_______,_______,_______,  _______,_______,
+      _______,KC_P1,KC_P2,KC_P3,KC_P4,KC_P5,                                    KC_P6,KC_P7,KC_P8,KC_P9,KC_P0,_______,
+      KC_NUM,KC_P4,KC_P5,KC_P6,KC_EQL,KC_MINS,                                 KC_MS_L,KC_MS_D,KC_MS_U,KC_MS_R,_______,_______,
+      _______,KC_P7,KC_P8,KC_P9,KC_P0,KC_PLUS,                                _______,_______,_______,_______,_______,_______,
       _______,_______,_______, _______,_______, _______,_______,     KC_MS_BTN1,ADJUST,KC_MS_BTN2,_______,_______, _______,_______
   ),
 
   [_ADJUST] = LAYOUT(
-      _______, KC_F1,KC_F2,   KC_F3,   KC_F4, KC_F5,                          _______,_______,KC_WH_U,_______, _______,KC_DEL,
-      KC_LCAP, KC_F6,KC_F7,KC_F8,KC_F9,KC_F10,                                _______, KC_HOME, KC_WH_D, KC_END, _______, _______,
-      _______, KC_INS, KC_PSCR, KC_PAUS, KC_F11, KC_F12,                      _______, _______, _______, _______,_______, _______,
-      _______, QK_BOOT, _______, _______, _______, _______, _______, _______, _______,  _______, _______,  _______, _______, _______
+      KC_F1,KC_F2,   KC_F3,   KC_F4, KC_F5,KC_F6,                             KC_F7,KC_F8,KC_F9,KC_F10,KC_F11, KC_F12,
+      _______, _______, _______, _______,_______, _______,                    KC_HOME, KC_WH_D,KC_WH_U, KC_END, KC_PAUS,_______,
+      _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______,_______, _______,
+      _______, QK_BOOT, KC_DEL, _______, _______, _______, _______,  _______, _______, KC_PSCR, _______,  _______, _______, _______
   ),
 };
 
